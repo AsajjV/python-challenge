4ea1fcdc89b59ce683ecf746460bba145795e4eb
Technomancer <ainemcbain@gmail.com>
2018-10-05 22:38:07 -0500

    calculate total months

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,11 @@ import csv
 budget_csv = os.path.join("budget_data.csv")
 with open(budget_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    
+
+    csvheader = next(csvreader)
+
+    num_months = len(list(csvreader))
+
 print ("Financial Analysis")
 print ("-------------------------")
-
+print (f"Total Months:  {num_months}")
