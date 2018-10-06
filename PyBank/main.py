bb33e53fa3bbf11435bafe6eb02820e2d9567967
Technomancer <ainemcbain@gmail.com>
2018-10-06 10:06:22 -0500

    total profit losses added

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,10 +4,28 @@ budget_csv = os.path.join("budget_data.csv")
 with open(budget_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    csvheader = next(csvreader)
+    csvheader = next(csvfile)
+    print (csvheader) 
+    
+    #Date=csvheader.index("Date")
+    #ProfitLosses=csvheader.index("Profit/Losses")
 
-    num_months = len(list(csvreader))
+    sum_profit = 0
+    sum_loss = 0
+    amount = 0
+
+    for row in csvreader:
+        amount = float(row[1])
+        if float(amount) > 0:
+            sum_profit = float(sum_profit) + float(amount)
+        else:
+            sum_loss = float(sum_loss) + float(amount)
+    
+    totalProfLoss=sum_loss + sum_profit
+    num_months = len(list(csv.reader(open('budget_data.csv'))))-1
+
 
 print ("Financial Analysis")
 print ("-------------------------")
 print (f"Total Months:  {num_months}")
+print (f"Total:  ${totalProfLoss}")
